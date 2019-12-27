ba51c46d924f69e43002e4c46f6dcc50d5d7e4de
Alwin Riady Argono <alwinriady@yahoo.com>
2019-12-26 18:27:59 -0800

    Add files via upload

---
+++ Dialisa Cognito/Register Page/js/config1.js
@@ -0,0 +1,8 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-2_wowJEwIKr', // e.g. us-east-2_uXboG5pAb
+        region: 'us-east-2', // e.g. us-east-2
+		clientId: '7ad0b7brqh0vss75clj29e4t7t' //is this used anywhere?
+    },
+};
+
